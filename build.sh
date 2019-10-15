daa0f2eed1a1a0d5b690e2d83aa5b7075364827b
ctjong <christophertjong@hotmail.com>
2019-10-15 01:38:54 -0700

    updated build script

--- build.sh
+++ build.sh
@@ -4,17 +4,21 @@ set -x #echo on
 basepath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd ${basepath}/core
-npm ci
 tsc --build tsconfig.json
+npm install
 
-cd ${basepath}/test/server
-rm orion-api*.tgz
-npm pack ../../core
-mv orion-api*.tgz orion.tgz
-npm ci
+if [ "$1" == "-testserver" ]; then
+    cd ${basepath}/test/server
+    rm orion-api*.tgz
+    npm pack ../../core
+    mv orion-api*.tgz orion.tgz
 
-if [ "$1" == "-test" ]; then
     cd ${basepath}/test
-    docker-compose stop
-    docker-compose up
+    if [ "$2" != "" ]; then
+        docker-compose -H $1 stop
+        docker-compose -H $1 up
+    else
+        docker-compose stop
+        docker-compose up
+    fi
 fi
